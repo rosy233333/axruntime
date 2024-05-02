3dbaf5eef9605d263932079aebd4a4e6cbc888a9
郑友捷 <Azure_stars@126.com>
2024-05-02 15:00:34 +0800

    feat: add the allocator init function

--- src/lib.rs
+++ src/lib.rs
@@ -131,6 +131,8 @@ pub extern "C" fn rust_main(cpu_id: usize, dtb: usize) {
             r.flags
         );
     }
+    #[cfg(feature = "alloc")]
+    init_allocator();
 
     #[cfg(feature = "paging")]
     {
@@ -189,6 +191,36 @@ pub fn exit_main() {
     {
         debug!("main task exited: exit_code={}", 0);
         axhal::misc::terminate();
+    }
+}
+
+#[cfg(feature = "alloc")]
+#[allow(dead_code)]
+fn init_allocator() {
+    use axhal::mem::{memory_regions, phys_to_virt, MemRegionFlags};
+
+    info!("Initialize global memory allocator...");
+    info!("  use {} allocator.", axalloc::global_allocator().name());
+
+    let mut max_region_size = 0;
+    let mut max_region_paddr = 0.into();
+    for r in memory_regions() {
+        if r.flags.contains(MemRegionFlags::FREE) && r.size > max_region_size {
+            max_region_size = r.size;
+            max_region_paddr = r.paddr;
+        }
+    }
+    for r in memory_regions() {
+        if r.flags.contains(MemRegionFlags::FREE) && r.paddr == max_region_paddr {
+            axalloc::global_init(phys_to_virt(r.paddr).as_usize(), r.size);
+            break;
+        }
+    }
+    for r in memory_regions() {
+        if r.flags.contains(MemRegionFlags::FREE) && r.paddr != max_region_paddr {
+            axalloc::global_add_memory(phys_to_virt(r.paddr).as_usize(), r.size)
+                .expect("add heap memory region failed");
+        }
     }
 }
 
