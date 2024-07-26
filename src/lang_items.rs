3ff4e76febe8b92ef6f502b1260d05f3fdc498a4
Youjie Zheng <Azure_stars@126.com>
2024-07-26 17:09:02 +0800

    chore: remove useless backtrace module

--- src/lang_items.rs
+++ src/lang_items.rs
@@ -3,10 +3,5 @@ use core::panic::PanicInfo;
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     error!("{}", info);
-    #[cfg(feature = "backtrace")]
-    {
-        extern crate axbacktrace;
-        axbacktrace::backtrace();
-    }
     axhal::misc::terminate()
 }
